36e3947097fb787ef93632fa517e153e3d0d117f
utouto97 <utouto97@gmail.com>
2024-04-16 17:38:26 +0900

    subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
+use rand::distributions::{Alphanumeric, DistString};
 
 #[derive(Debug, Parser)]
-struct Args {
-    #[arg(help = "name")]
-    name: String,
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(about = "new note")]
+    New(NewArgs),
+}
+
+#[derive(Debug, Args)]
+struct NewArgs {
+    #[arg(help = "note title")]
+    title: String,
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("Hello, {}!", args.name);
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::New(args) => {
+            let id = generate_id();
+            println!("Creating new note with id: {}, title: {}", id, args.title);
+        }
+    }
+}
+
+fn generate_id() -> String {
+    let mut rng = rand::thread_rng();
+    Alphanumeric.sample_string(&mut rng, 32)
 }
