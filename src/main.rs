53d88a8ce738270fc5df65427d1e58e54aca10ca
utouto97 <utouto97@gmail.com>
2024-04-16 17:56:49 +0900

    ファイルの作成

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,42 @@ struct NewArgs {
     title: String,
 }
 
-fn main() {
-    let cli = Cli::parse();
-    match cli.command {
-        Commands::New(args) => {
-            let id = generate_id();
-            println!("Creating new note with id: {}, title: {}", id, args.title);
-        }
-    }
+struct Config {
+    notes_dir: String,
+}
+
+struct App {
+    config: Config,
 }
 
 fn generate_id() -> String {
     let mut rng = rand::thread_rng();
     Alphanumeric.sample_string(&mut rng, 32)
+}
+
+impl App {
+    fn new(config: Config) -> Self {
+        App { config }
+    }
+
+    fn new_note(&self, id: &str, title: &str) -> anyhow::Result<()> {
+        let filename =
+            std::path::Path::new(self.config.notes_dir.as_str()).join(format!("{}.md", id));
+        std::fs::write(&filename, title)?;
+        Ok(())
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let app = App::new(Config {
+        notes_dir: String::from("./notes"),
+    });
+    match cli.command {
+        Commands::New(args) => {
+            let id = generate_id();
+            println!("Creating new note with id: {}, title: {}", id, args.title);
+            app.new_note(&id, &args.title).unwrap();
+        }
+    }
 }
